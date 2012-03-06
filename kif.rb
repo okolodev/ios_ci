788217829adf816628941dd3c3a5b3174e3f47bd
Alexey Denisov <denisov@mlsdev.com>
2012-03-06 18:31:56 +0200

    KIF script added

--- kif.rb
+++ kif.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/ruby
 require "getoptlong"
 
+source_root = nil
+target = nil
+arch = "iphoneos"
+configuration = "Release"
+submodules = "submodules"
+sdk = nil
+family = nil
+log_file = nil
+sim_path = "/usr/local/bin/ios-sim"
+
 getoptlong = GetoptLong.new(
    [ '--sdk',   '-s', GetoptLong::REQUIRED_ARGUMENT ],
    [ '--target',  '-t', GetoptLong::REQUIRED_ARGUMENT ],
@@ -8,33 +18,31 @@ getoptlong = GetoptLong.new(
    [ '--log-file', '-l', GetoptLong::REQUIRED_ARGUMENT ],
    [ '--source-root', '-r', GetoptLong::REQUIRED_ARGUMENT ],
    [ '--arch', '-a' ,GetoptLong::REQUIRED_ARGUMENT ],
-   [ '--family', '-f' ,GetoptLong::REQUIRED_ARGUMENT ]
-
+   [ '--family', '-f' ,GetoptLong::REQUIRED_ARGUMENT ],
+   [ '--submodules', '-m' ,GetoptLong::REQUIRED_ARGUMENT ]
 )
 
-def printUsage()
+def printUsage
 	puts "Usage:"
-	puts "cedar.rb --source-root source_path --target project_target [ --sdk sdk] [ --family device_family ] [ --configuration config ] [ --arch arch] [ --log-file log_path ]"
+	puts "kif.rb --source-root source_path --target project_target [ --submodules submodules ] [ --sdk sdk] [ --family device_family ] [ --configuration config ] [ --arch arch] [ --log-file log_path ]"
         puts "  --arch: iphoneos, iphonesimulator"
         puts "    default: iphoneos"
         puts "  --configuration: Release, Debug"
         puts "    default: Release"
+        puts "  --submodules: submodules directory"
+        puts "    default: submodules/"
         puts "  --sdk: version"
         puts "    default: latest"
         puts "  --family: iphone, ipad"
         puts "    default: iphone"
         puts "  --log-file: path to log"
-        puts "    default: /tmp/cedar-$target-$timestamp.log"
+        puts "    default: /tmp/kif-$target-$timestamp.log"
 end
 
-source_root = nil
-target = nil
-arch = "iphoneos"
-configuration = "Release"
-sdk = nil
-family = nil
-log_file = nil
-sim_path = "/usr/local/bin/ios-sim"
+def printLog
+  end
+
+
 
 begin
   getoptlong.each do |opt, arg|
@@ -53,6 +61,8 @@ begin
         log_file = arg
       when "--family"
         family = arg
+      when "--submodules"
+        submodules = arg
     end
   end
 rescue StandardError=>my_error_message
@@ -69,19 +79,49 @@ if target.nil? or source_root.nil?
   exit 1
 end
 
-log_file = "/tmp/cedar-#{target}-#{Time.now.to_i}.log" if log_file.nil?
+log_file = "/tmp/kif-'#{target}'-#{Time.now.to_i}.log" if log_file.nil?
+
+kif = "xcodebuild  -project '#{source_root}/#{submodules}/KIF/KIF.xcodeproj' -target 'KIF' -sdk #{arch} build"
+kif_exit_code = system(kif)
+
+if kif_exit_code
+  puts "KIF library build succeeded"
+  puts
+else
+  puts "KIF library build failed"
+  exit 1
+end
+
+%x[ cd #{source_root} ]
+test_target = "xcodebuild -target '#{target}' -sdk #{arch} -configuration #{configuration} build"
+puts "#{test_target}"
+test_target_exit_code = system(test_target)
+
+if test_target_exit_code
+  puts "KIF tests build succeeded"
+  puts
+else
+  puts "KIF tests build failed"
+  exit 1
+end
+
 app_path = "#{source_root}/build/#{configuration}-#{arch}/#{target}.app"
 
 %x[ killall "iPhone Simulator" ]
-%x[ cd #{source_root} ]
 
-test_command = "#{sim_path} launch #{app_path} --setenv CEDAR_HEADLESS_SPECS=1 --setenv CEDAR_REPORTER_CLASS=CDRColorizedReporter,CDRJUnitXMLReporter --setenv CEDAR_JUNIT_XML_FILE=#{source_root}/test-reports/cedar.xml "
+test_command = "#{sim_path} launch '#{app_path}'"
 test_command + " --family #{family} " unless family.nil?
 test_command + " --sdk #{sdk} " unless sdk.nil?
 
+puts test_command
+
 system("#{test_command} > #{log_file} 2>&1")
 
-grep_exit_code = system("grep -q \"0 failures\" #{log_file}")
+grep_exit_code = system("grep -q \"KIF TESTING FINISHED: 0 failures\" #{log_file}")
+
+app_path = "/Users/Shared/Jenkins/Library/Application Support/iPhone Simulator/5.0/Applications/"
+%x[ killall "iPhone Simulator" ]
+system("rm -rf '#{app_path}'")
 
 File.open(log_file, 'r') do |f1|
   while line = f1.gets
