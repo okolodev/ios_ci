8808ee3ea3679e369b948f7a3b92a65231e48a1e
Alexey Belkevich <belkevich@mlsdev.com>
2012-11-27 15:17:26 +0200

    Fixed 'print_file' exception

--- classes/print_file.rb
+++ classes/print_file.rb
@@ -1,9 +1,13 @@
 def print_file(file)
   if !file.nil?
-    File.open(file, 'r') do | f |
-      while line = f.gets
-        puts line
+    if File.exist?(file)
+      File.open(file, 'r') do | f |
+        while line = f.gets
+          puts line
+        end
       end
+    else
+      puts "File not exists:\n#{file}"
     end
   end
 end
